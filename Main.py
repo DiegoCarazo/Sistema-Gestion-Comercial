967cdeaa02f2c01704b00311706f4c5fbf925ac3
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-06-15 08:39:00 -0600

    Create Main.py
    Main created

---
+++ Main.py
@@ -0,0 +1,36 @@
+# Gestión de Información
+    #Registro de usuarios
+
+    #Gestión de clientes
+
+    #Registro de proveedores 
+
+    #Cálculo del IVA:
+
+    #Valores de IVA configurables
+
+    #Cálculo del IVA
+    
+# Gestión de Inventarios de Ventas:
+    #Registro de ventas
+
+    #Actualización de inventario
+
+    #Control de stock
+    
+# Generación de Informes:
+    #Informes de ventas
+
+    #Informes de inventario
+
+    #Informes de clientes
+
+    #Registro de productos
+
+#Sistema de Pagos
+    #Registro de métodos de pago
+
+    #Selección de método de pago
+
+    #Integración de correo electrónico
+
