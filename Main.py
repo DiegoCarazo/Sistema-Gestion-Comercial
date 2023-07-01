6b04818a88b842c185fb4b773e790fa0625a6bdd
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-07-01 07:54:42 -0600

    Se agraga el IVA

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
-#Librerias
+# Librerías
 import json
+
+
 # Gestión de Información
 
-#Registro de usuarios
+# Registro de usuarios
 def personal():
     nombre = input('Nombre: ')
     correo = input('Correo: ')
@@ -16,7 +18,8 @@ def personal():
 
     return temp_dic
 
-#Gestión de clientes
+
+# Gestión de clientes
 def clientes():
     nombre = input('Nombre: ')
     correo = input('Correo: ')
@@ -29,7 +32,8 @@ def clientes():
 
     return temp_dic
 
-#Registro de proveedores 
+
+# Registro de proveedores 
 def proveedores():
     nombre = input('Nombre: ')
     correo = input('Correo: ')
@@ -41,38 +45,54 @@ def proveedores():
     temp_dic.update({nombre: lista_datos})
 
     return temp_dic
+
+
 dic_personal = personal()
 
-#uso del gestion de información
+# uso del gestion de información
 with open('Datos personal.jason', 'a') as f:
     json.dump(dic_personal, f, indent=4)
 
-#Cálculo del IVA:
-
-    #Valores de IVA configurables
-
-    #Cálculo del IVA
-    
 # Gestión de Inventarios de Ventas:
-    #Registro de ventas
+# Registro de ventas
 
-    #Actualización de inventario
+# Actualización de inventario
 
-    #Control de stock
-    
+# Control de stock
+inventario = {
+    # articulo : [Precio, proveedor, IVA]
+    'Camiseta': [5500, 'Nike', 0.13],
+    'Pantalones': [12000, 'Adidas', 0.04],
+    'Proteína': [35000, 'Builder', 0.02],
+    'Pesas': [40000, 'Builder', 0.01]
+}
+
+
+# Cálculo del IVA:
+# Valores de IVA configurables | IVA localizado en el inventario
+def calculo_iva():
+    print(inventario)
+    articulo = input('Escriba el nombre: ')
+    iva = inventario[articulo][2]
+    precio = inventario[articulo][0]
+    calculo = precio + precio * iva
+    print(calculo)
+
+
+calculo_iva()
+
 # Generación de Informes:
-    #Informes de ventas
+# Informes de ventas
 
-    #Informes de inventario
+# Informes de inventario
 
-    #Informes de clientes
+# Informes de clientes
 
-    #Registro de productos
+# Registro de productos
 
-#Sistema de Pagos
-    #Registro de métodos de pago
+# Sistema de Pagos
+# Registro de métodos de pago
 
-    #Selección de método de pago
+# Selección de método de pago
 
-    #Integración de correo electrónico
-
+# Integración de correo electrónico
