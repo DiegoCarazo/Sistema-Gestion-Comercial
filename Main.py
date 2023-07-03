5e18dea6c2537b6ec8575530216f4d65c2c52f4a
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-07-02 20:45:47 -0600

    generar informes de ventas

--- Main.py
+++ Main.py
@@ -52,6 +52,20 @@ calculo_iva()
 # ----- Gestión de Inventarios de Ventas -----
 
 # ----- Generación de Informes ----- 
+def generar_informe_ventas():
+#imprimir encabezado del informe de ventas
+    print("Informe de Ventas")
+#buscar la venta en la lista de ventas
+    for venta in ventas:
+#obtener los valores de cada venta 
+        producto = venta["producto"]
+        cantidad = venta["cantidad"]
+        precio = venta["precio"]
+        monto_total = cantidad * precio
+#imprimir el informe        
+print("Producto:", producto)
+print("Cantidad vendida:", cantidad)
+print("Monto total:", monto_total)
 
 # ----- Sistema de Pagos -----
 
