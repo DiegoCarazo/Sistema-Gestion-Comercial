76f023ab3cc801832afafee23cedeaf571f798e6
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-07-03 19:50:30 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -50,6 +50,52 @@ def calculo_iva():
 calculo_iva()
 
 # ----- Gestión de Inventarios de Ventas -----
+print("Bienvenido al Programa de Gestión de Inventarios: ")
+cantidades = []
+productos = []
+precios = []
+
+while True:
+    print("""
+    (1) Añadir productos
+    (2) Buscar productos
+    (3) Modificar productos
+    (4) Ver productos
+    (5) Salir
+    """)
+
+    respuesta = int(input("Ingresa una de las opciones: "))
+
+    if respuesta == 1:
+        cantidad = int(input("Ingresa la cantidad del producto: "))
+        producto = input("Ingresa el nombre del producto: ")
+        precio = int(input("Ingresa el precio del producto: "))
+
+        cantidades.append(cantidad)
+        productos.append(producto)
+        precios.append(precio)
+    elif respuesta == 2:
+        buscador = input("Ingresa el nombre del producto que deseas buscar: ")
+        posicion = productos.index(buscador)
+        print("La cantidad del producto es: ", cantidades[posicion])
+        print("El nombre del producto es: ", productos[posicion])
+        print("El precio del producto es: ", precios[posicion])
+    elif respuesta == 3:
+        buscador = input("Ingresa el nombre del producto que deseas modificar: ")
+        posicion = productos.index(buscador)
+        cantidad = int(input("Ingresa la cantidad del producto: "))
+        producto = input("Ingresa el nombre del producto: ")
+        precio = int(input("Ingresa el precio del producto: "))
+        cantidades[posicion] = cantidad
+        productos[posicion] = producto
+        precios[posicion] = precio
+    elif respuesta == 4:
+        print("La cantidad es: ", cantidades)
+        print("El nombre es: ", productos)
+        print("El precio es: ", precios)
+    else:
+        break
+
 
 # ----- Generación de Informes ----- 
 def generar_informe_ventas():
