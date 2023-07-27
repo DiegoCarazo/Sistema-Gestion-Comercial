e6ba25b02d2670d467a8684221a371389aa8eefc
DiegoCarazo <126536443+DiegoCarazo@users.noreply.github.com>
2023-07-27 15:54:28 -0600

    Delete inventario.py

--- inventario.py
+++
@@ -1,7 +0,0 @@
-inventario = {
-    'El Gran Gatsby': [25000, 'F. Scott Fitzgerald', 0.13, 'Considerada como una de las mejores novelas americanas del siglo XX.'],
-    '1984': [18000, 'George Orwell', 0.10, 'Una distopía que presenta una sociedad totalitaria y vigilante.'],
-    'Orgullo y prejuicio': [20000, 'Jane Austen', 0.04, 'Una novela de amor y crítica social ambientada en la sociedad británica del siglo XIX.'],
-    'Cien años de soledad': [30000, 'Gabriel García Márquez', 0.01, 'Una obra maestra del realismo mágico que narra la historia de la familia Buendía.'],
-    'Don Quijote de la Mancha': [35000, 'Miguel de Cervantes Saavedra', 0.13, 'Una de las obras más importantes de la literatura española.'],
-}
